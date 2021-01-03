ba567b4c851716a939de13cf7548d1043404b576
Hasheem <hasheemhush@gmail.com>
2021-01-03 14:04:02 +0530

    Updates

---
+++ precache-manifest.7ee77befc0563716470c4f3c68b757c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac65cb039e61fcb1cd29771eda470be6",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "7f7086f6f78db82b70cd",
+    "url": "/landingpage/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e7006c29ad561a8c285a",
+    "url": "/landingpage/static/js/2.8ec8fc10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landingpage/static/js/2.8ec8fc10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7086f6f78db82b70cd",
+    "url": "/landingpage/static/js/main.0b480eb5.chunk.js"
+  },
+  {
+    "revision": "f1659dd5f97b7792f2f0",
+    "url": "/landingpage/static/js/runtime-main.6b3a62fa.js"
+  }
+]);
