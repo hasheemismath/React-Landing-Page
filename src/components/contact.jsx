451cc1c58ec6bb6447dae6311b75bf54e3fcb721
Hasheem <hasheemhush@gmail.com>
2021-01-03 15:21:11 +0530

    footer updated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -117,9 +117,9 @@ export class Contact extends Component {
         <div id="footer">
           <div className="container text-center">
             <p>
-              &copy; 2020 Issaaf Kattan React Land Page Template. Design by{" "}
-              <a href="http://www.templatewire.com" rel="nofollow">
-                TemplateWire
+              &copy; React Land Page Template. Design by{" "}
+              <a href="https://hasheem.me" rel="nofollow">
+                HASHEEM 
               </a>
             </p>
           </div>
